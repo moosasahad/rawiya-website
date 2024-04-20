8e69a3ad0aad74c76ea9f0c2e51b38c41d0c2416
moosasahad <165463409+moosasahad@users.noreply.github.com>
2024-04-20 22:41:04 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,15 @@
+function changetextline1(text)
+{
+    var changetext = document.getElementById('changetext');
+    changetext.innerHTML = 'new text add';
+}
+function changetextline2(text)
+{
+    var changetext = document.getElementById('changetext');
+    changetext.innerHTML = 'new text add2';
+}
+function changetextline3(text)
+{
+    var changetext = document.getElementById('changetext');
+    changetext.innerHTML = 'new text add3';
+}
